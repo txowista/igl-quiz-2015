377b5d923b7e214283ea6e71852e1b0da9fed2b3
Igor Gonzalez <txowista@gmail.com>
2015-07-10 09:14:06 +0200

    CSS adaptado para moviles

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -7,6 +7,6 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 router.get('/quizes/question', quizController.question);
-router.get('quizes/answer',quizController.answer);
+router.get('/quizes/answer',quizController.answer);
 
 module.exports = router;
