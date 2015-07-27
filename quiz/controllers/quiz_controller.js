8a0d582f67000f8c86fe8f574ffbb6ecd0e6125b
Igor Gonzalez <txowista@gmail.com>
2015-07-27 09:36:36 +0200

    Ejercicio de busqueda P2P

--- quiz/controllers/quiz_controller.js
+++ quiz/controllers/quiz_controller.js
@@ -1,12 +1,45 @@
-//GET /quizes/question
-exports.question=function(req, res){
-  res.render('quizes/question',{pregunta:'Capital de Italia'});
+var models = require('../models/models.js');
+exports.load= function(req,res,next,quizId){
+  models.Quiz.findById(quizId).then(
+      function(quiz){
+        if(quiz){
+          req.quiz=quiz;
+          next();
+        }else{
+          next(new Error('No existe quizId=' + quizId));
+        }
+      }
+  ).catch(function(error){next(error);});
 };
+//GET /author
+exports.author = function(req, res){
+    res.render('author', {autor: 'Igor Gonzalez'})
+};
+exports.index = function(req, res){
+    if(req.query.search) {
+        var filtro = (req.query.search || '').replace(" ", "%");
+        models.Quiz.findAll({where:["pregunta like ?", '%'+filtro+'%'],order:'pregunta ASC'}).then(function(quizes){
+            res.render('quizes/index', {quizes: quizes});
+        }).catch(function(error) { next(error);});
+
+    } else {
+
+        models.Quiz.findAll().then(function(quizes){
+            res.render('quizes/index', {quizes: quizes});
+        }).catch(function(error) { next(error);});
+    }
+};
+//GET /quizes/question
+exports.show=function(req, res) {
+      res.render('quizes/show', {quiz: req.quiz});
+  };
+
 //GET /quizes/answer
 exports.answer=function(req, res){
-  if(req.query.respuesta==='Roma'){
-    res.render('quizes/answer',{respuesta: 'Correcto'});
-  }else {
-    res.render('quizes/answer',{respuesta:'Incorrecto'});
-  }
+  var resultado = 'Incorrecto';
+   if (req.query.respuesta === req.quiz.respuesta) {
+     resultado='Correcto';
+    }
+  res.render('quizes/answer', {quiz:req.quiz,respuesta: resultado});
 };
+
