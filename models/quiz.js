304d958b60f6bd062fd0a3e05dd93f2da9b653b0
Igor Gonzalez <txowista@gmail.com>
2015-07-28 09:18:44 +0200

    De nuevo problemas coon git

--- models/quiz.js
+++
@@ -1,7 +0,0 @@
-module.exports=function(sequelize, DataTypes){
-  return sequelize.define('Quiz',
-        {
-          pregunta: DataTypes.STRING,
-          respuesta: DataTypes.STRING,
-        });
-}
