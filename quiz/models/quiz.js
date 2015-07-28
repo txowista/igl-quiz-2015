1e39e5d6ae8448f4751a0fcda3e6597a20180ccf
Igor Gonzalez <txowista@gmail.com>
2015-07-28 10:11:34 +0200

    Desperado con GIT

--- quiz/models/quiz.js
+++
@@ -1,9 +0,0 @@
-module.exports=function(sequelize, DataTypes){
-  return sequelize.define('Quiz',
-        {
-          pregunta: DataTypes.STRING,
-          respuesta: DataTypes.STRING,
-        },
-
-      { tableName: 'Quiz'});
-}
