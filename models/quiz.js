e3421e0a9ac0e7521fd222030f19485831527a2e
Igor Gonzalez <txowista@gmail.com>
2015-07-28 10:09:11 +0200

    Problemas con Git no hago carrera

--- models/quiz.js
+++ models/quiz.js
@@ -1,14 +1,14 @@
-module.exports=function(sequelize, DataTypes) {
-    return sequelize.define('Quiz',
+module.exports=function(sequelize, DataTypes){
+  return sequelize.define('Quiz',
         {
-            pregunta: {
-                type: DataTypes.STRING,
-                validate: {notEmpty: {msg: "-> Falta Pregunta"}}
-            },
-            respuesta: {
-                type: DataTypes.STRING,
-                validate: {notEmpty: {msg: "-> Falta respuesta"}}
-            }
+          pregunta:{
+              type:DataTypes.STRING,
+              validate:{notEmpty:{msg: "-> Falta Pregunta"}}
+          },
+          respuesta: {
+              type:DataTypes.STRING,
+              validate:{notEmpty:{msg: "-> Falta respuesta"}}
+          }
         }
-    );
+  );
 }
